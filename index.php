f96d1a342ccf9bfdceb33386102a3d11230051bd
eltonanit <elton.ani@gmail.com>
2024-09-23 17:10:52 +0200

    [UPD] Added class Product

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+     class Product {
+        public $name;
+        public $price;
+        public $image;
+        public $quantity;
+        public $category;
+        public $description;
+ 
+     }
+ 
+?>
