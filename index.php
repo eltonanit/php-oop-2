d282dec97a2ef0a245b0278ec4246707fd4cf9d3
eltonanit <elton.ani@gmail.com>
2024-09-23 17:22:18 +0200

    [UPd]Added construct

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
         public $quantity;
         public $category;
         public $description;
- 
+
+        function __construct($_name, $_price, $_image, $_quantity = 10, Category $_category, $_description){
+            $this->name = $_name;
+            $this->image = $_image;
+            $this->price = $_price;
+            $this->quantity = $_quantity;
+            $this->category = $_category;
+            $this->description = $_description;
+            
+        }
+
+     
      }
- 
+
+   
 ?>
