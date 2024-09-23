edc3ee1a70212da981e4477d243566199d0d125b
eltonanit <elton.ani@gmail.com>
2024-09-23 18:27:10 +0200

    [UPD] Added Food class

---
+++ Models/Food.php
@@ -0,0 +1,20 @@
+<?php
+     require_once __DIR__.'Product.php';
+
+     class Food extends Product {
+        protected $calories; 
+
+        function __construct($_name, $_image, $_price, $_quantity = 10, $_catwgory ){
+            parent::__construct($_name, $_image, $_price, $_quantity, $_catwgory);
+        
+        }
+        public function setCalories ($_calories) {
+            $this->calories = $_calories;
+        }
+         public function setCalories () {
+            return $this->calories;
+        }
+
+     }
+
+?>
