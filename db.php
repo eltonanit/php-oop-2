92c2c489dd78beb4d6d5b0deb1b71322c83807be
eltonanit <elton.ani@gmail.com>
2024-09-24 18:50:22 +0200

    [UPD] Addded try - catch

--- db.php
+++ db.php
@@ -12,9 +12,19 @@
      $dogs = new Category ('Cane', 'fa-solid fa-dog');
      $cats = new Category ('Gatto', 'fa-solid fa-cat');
      
-     $product = new Product ('Collare', 'https://pawee.it/wp-content/uploads/2022/07/WhatsApp-Image-2022-07-06-at-13.59.57.jpeg', 12.00, 20, true, $dogs);
-     $product->description = "Lorem ipsum ";
+     $products = [];
+    
+     try {
+          $product = new Product ('Collare', 'https://pawee.it/wp-content/uploads/2022/07/WhatsApp-Image-2022-07-06-at-13.59.57.jpeg', 12.00, 20, true, $dogs);
+          $product->description = "Lorem ipsum ";
+          $products [] = $product;
 
+
+     }
+     catch (Exception $ex) {
+      echo "----ERRORE----".$ex->getMessage();
+     }
+ 
     $food = new Food('Mais','https://www.verdepiusnc.it/wp-content/uploads/2016/06/Sweet-Corn.jpg' , 9.00, 30,true, $dogs );
     $food->setCalories(500);
 
@@ -24,11 +34,11 @@
     $bed = new Bed('Dado','https://p.turbosquid.com/ts-thumb/Fq/WXForr/RcEnHqbr/cube/jpg/1577408929/600x600/fit_q87/85c37a88b9ec281c80c7ad5559994cc9c14cd59e/cube.jpg' , 15.00, 30, true, $cats );
     $bed->setSize('400x20x300');
 
-     $products = [
-        $product,
-        $game,
-        $bed,
-        $food
-     ];
+     $products [] = $game ;
+      $products  [] = $bed ;
+       $products  [] = $food ;
+      
+        
+     
 
 ?>
